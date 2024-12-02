117941e4a700e340f9e15acc578b660821786e43
Hasan Gül <hsngl695@gmail.com>
2024-12-02 18:52:45 +0300

    prepared some ui

--- FocusOn/app/src/main/java/mad/focuson/MainView.java
+++ FocusOn/app/src/main/java/mad/focuson/MainView.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
 }
