49e496a87b29b32f78a3fa07154da447fa0be9e9
Hasan Gül <hsngl695@gmail.com>
2024-12-06 20:55:43 +0300

    added more ui components

---
+++ FocusOn/app/src/main/java/mad/focuson/Music.java
@@ -0,0 +1,24 @@
+package mad.focuson;
+
+import java.io.Serializable;
+
+public class Music implements Serializable {
+    String name;
+    String path;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
--- FocusOn/app/src/main/java/mad/focuson/TasksView.java
+++ FocusOn/app/src/main/java/mad/focuson/TasksView.java
@@ -1,12 +1,17 @@
 package mad.focuson;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class TasksView extends AppCompatActivity {
 
@@ -15,10 +20,21 @@ public class TasksView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tasks_view);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tasks), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button btn = findViewById(R.id.btnAddNewTask);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.fragmentContainerView, TaskSettingsFragment.newInstance(null, null));
+                ft.commit();
+                btn.setText("Save");
+            }
         });
     }
 }
